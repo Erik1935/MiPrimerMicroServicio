80ff06fe0c88e9ad8938587ef665e15fc9ae28ba
Erik1935 <erikjose1935@hotmail.com>
2023-10-06 15:52:07 -0600

    Contenido inicial

---
+++ products-service/src/main/java/com/erik1935/productsservice/ProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erik1935.productsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+}
